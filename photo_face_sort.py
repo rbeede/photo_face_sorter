eb1d68b19b48052d237560fbb65257deb47beba0
Rodney Beede <rbeede@users.noreply.github.com>
2022-04-17 17:02:23 -0500

    Untested revision

--- photo_face_sort.py
+++ photo_face_sort.py
@@ -6,7 +6,9 @@
 
 
 # Based off https://github.com/ageitgey/face_recognition/issues/359
-# but kept it to using only a single CPU so running on a Raspberry Pi or other shared-system would not consume all the CPU. It takes longer but only uses 1 core on a multi-user system.
+# but kept it to using only a single CPU so running on a Raspberry Pi
+# or other shared-system would not consume all the CPU.
+# It takes longer but only uses 1 core on a multi-user system.
 
 
 # Raspberry Pi 4+ install trick
@@ -27,40 +29,48 @@ from pathlib import Path
 import glob
 
 
-faces = []
 
 # If filename has a face (only looks at first face in the image) that has been seen before
 # then it returns the index code for that face
 # If filename has no recognized faces then it returns the last index of faces where the new face is stored
-def add_faces(filename):
+def add_faces(filename, existing_faces):
 	image = face_recognition.load_image_file(filename)
 	encoding = face_recognition.face_encodings(image)[0]
 
 
-	for i, existing_encoding in enumerate(faces):
+	for i, existing_encoding in enumerate(existing_faces):
 		if face_recognition.compare_faces([existing_encoding], encoding)[0]:
 			return i  # The face in filename matched a previous filename's face so return the index code
 
-	faces.append(encoding)
-	return len(faces) - 1  # Never seen a matching face, new face so return last index where it is stored
-	
-	
+	# Never seen a matching face (new face) so return last index where it is stored
+	existing_faces.append(encoding)
+	return len(existing_faces) - 1
+
+
 def main(args):
 	print(f"Searching directory {args.directory} for images and faces")
-	
+
+	faces = []
+
 	for photo_file in Path(args.directory).rglob('*'):
 		# We only want to iterate once so check for multiple file-types here
 		if not photo_file.suffix.lower() in ['.png','.jpg']:
 			continue
 		else:
 			print(f"Looking at file {photo_file}")
-	
+
+			# modifies global  faces[]
+			face_idx = add_faces(photo_file, faces)
+
+			print(f"{photo_file}\t{face_idx}")
+
+
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description='https://www.rodneybeede.com/')
-	
+
 	parser.add_argument('--directory', required=True, action='store', type=Path, help='Directory to recurse for image files')
-	
+
 	args = parser.parse_args()
-	
+
 	main(args)
